f55d1be1aad997c92ad9ae52a35fabb3804f0bb8
tania.voyat <tetiana.voyat@gmail.com>
2020-07-17 00:21:17 +0300

    adds timer task

---
+++ index.js
@@ -0,0 +1,35 @@
+const refs = {
+  days: document.querySelector('span[data-value="days"]'),
+  hours: document.querySelector('span[data-value="hours"]'),
+  mins: document.querySelector('span[data-value="mins"]'),
+  secs: document.querySelector('span[data-value="secs"]'),
+};
+
+function CountdownTimer() {
+  const targetDate = new Date('Jul 17, 2021');
+  setInterval(() => {
+    const currentTime = Date.now();
+    const deltaTime = targetDate - currentTime;
+    updateTimer(deltaTime);
+  }, 1000);
+}
+
+CountdownTimer();
+
+function updateTimer(time) {
+  const days = pad(Math.floor(time / (1000 * 60 * 60 * 24)));
+  const hours = pad(
+    Math.floor((time % (1000 * 60 * 60 * 24)) / (1000 * 60 * 60)),
+  );
+  const mins = pad(Math.floor((time % (1000 * 60 * 60)) / (1000 * 60)));
+  const secs = pad(Math.floor((time % (1000 * 60)) / 1000));
+
+  refs.days.textContent = `${days}`;
+  refs.hours.textContent = `${hours}`;
+  refs.mins.textContent = `${mins}`;
+  refs.secs.textContent = `${secs}`;
+}
+
+function pad(value) {
+  return String(value).padStart(2, '0');
+}
